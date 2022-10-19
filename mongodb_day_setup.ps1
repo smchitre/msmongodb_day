38f33ab593918db9511aa63d5ddd0a2162df9a2a
unknown <smchitre@gmail.com>
2022-10-19 20:13:19 +0000

    20221020-smchitre-Fix powershell issues

--- mongodb_day_setup.ps1
+++ mongodb_day_setup.ps1
@@ -1,15 +1,15 @@
 Set-ExecutionPolicy Unrestricted 
 
- $TerraformDestination="$((New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path) + terraform.zip"                                                                                        
+ $TerraformDestination="$((New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path)\terraform.zip"                                                                                        
 
 Start-BitsTransfer -Source 'https://releases.hashicorp.com/terraform/1.3.2/terraform_1.3.2_windows_386.zip' -Destination $TerraformDestination
 
- $MongoshDestination="$((New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path) + mongosh.msi"     
+ $MongoshDestination="$((New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path)\mongosh.msi"     
 
 Start-BitsTransfer -Source 'https://downloads.mongodb.com/compass/mongosh-1.6.0-x64.msi?_ga=2.153152699.2061862758.1666074328-318488355.1664437022' -Destination $MongoshDestination
 
 
-$MongoCompassDestination="$((New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path) + mongocompass.msi"     
+$MongoCompassDestination="$((New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path)\mongocompass.msi"     
 
 Start-BitsTransfer -Source 'https://downloads.mongodb.com/compass/mongosh-1.6.0-x64.msi?_ga=2.153152699.2061862758.1666074328-318488355.1664437022' -Destination $MongoCompassDestination
 
@@ -18,5 +18,6 @@ Invoke-CimMethod -ClassName Win32_Product -MethodName Install -Arguments @{Packa
 Invoke-CimMethod -ClassName Win32_Product -MethodName Install -Arguments @{PackageLocation=$MongoshDestination}
 
 
-Expand-Archive  $TerraformDestination  -DestinationPath "$((New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path) + \terraform" 
+$TerraformFolder="$((New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path)\terraform" 
+Expand-Archive  $TerraformDestination  -DestinationPath $TerraformFolder -force
 
